f4af92416ef6802347fe9ce76d8a423238f224a9
Theodore John Josef <john.josef@aol.com>
2022-11-11 17:56:29 -0800

    commit tree 947765c773730ead3b4f1743fbfeef4cc283d18b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter ( find_name => {
+        return (find_name.toLowerCase() === name.toLowerCase())
+     })
+  }
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter ( find_name => {
+        if (letter.charAt(0) === find_name.charAt(0)) {
+            return find_name;
+        }
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter ( function(find_name){
+      if (find_name.name === name) {
+        return find_name;
+      }
+    })
+  }
